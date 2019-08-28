d4be712f2d5f9fdec4dc78b5e87f08f3a787d1f3
alaa-yasin <alaayasin.ps@gmail.com>
2019-08-28 16:20:13 +0300

    validate the signup form from the server Relates #34

--- src/controllers/postSignup.js
+++ src/controllers/postSignup.js
@@ -1,15 +1,25 @@
 const bycrpt = require('bcryptjs');
+const Joi = require('@hapi/joi');
 const { postSignUp } = require('../model/queries/postSignUp');
+const { schema } = require('./validation/signup');
 
 exports.postSignup = (req, res, next) => {
-  bycrpt.genSalt(10)
-    .then((salt) => bycrpt.hash(req.body.password, salt))
-    .then((hash) => {
-      console.log(hash);
-      req.body.password = hash;
-      postSignUp(req.body);
-    })
-    .then((r) => {
-      res.redirect('/');
-    });
+  Joi.validate(req.body, schema, (err, value) => {
+    if (err) {
+      res.render('signup', {
+        error: err.details[0].message,
+      });
+    } else {
+      bycrpt.genSalt(10)
+        .then((salt) => bycrpt.hash(req.body.password, salt))
+        .then((hash) => {
+          console.log(hash);
+          req.body.password = hash;
+          postSignUp(req.body);
+        })
+        .then((r) => {
+          res.redirect('/');
+        });
+    }
+  });
 };
