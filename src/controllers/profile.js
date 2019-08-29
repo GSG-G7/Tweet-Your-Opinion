36703cbfc479b96e02e4f3b18d3b7328abd8390a
FadiAlamassi <alamassifadi@gmail.com>
2019-08-29 10:10:50 +0300

    Add basic style display posts at home and profile pages relates #52

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const { getUserId } = require('../model/queries/queryAddPost');
+const getUserPost = require('../model/queries/getUserPost');
 require('env2')('config.env');
 
 
@@ -6,6 +8,12 @@ exports.profile = (req, res, next) => {
   jwt.verify(req.cookies.login, process.env.SECRET, (err, rest) => {
     if (err) {
       res.redirect('/login');
-    } else { res.render('profile', { cook: rest.username }); }
+    } else {
+      getUserId(rest.username)
+        .then((idRow) => idRow.rows[0].user_id)
+        .then((id) => getUserPost(id))
+        .then((postRow) => postRow.rows)
+        .then((postsArray) => res.render('profile', { cook: rest.username, postsArray }));
+    }
   });
 };
